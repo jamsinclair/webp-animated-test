2ffc2a2ae0a4e2e004ba3d4c649ce9a896326671
Jamie Sinclair <jamsinclairnz@gmail.com>
2025-02-16 13:24:00 +0900

    fix gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/webp-animated-test/',
+  build: {
+    outDir: 'docs'
+  },
   optimizeDeps: {
     exclude: ["@jsquash/webp"]
   }
